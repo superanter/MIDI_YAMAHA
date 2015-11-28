68a9fb7b21f180f6489a7f2710f2c4c4f3dbb06a
Yupo <yupotown@gmail.com>
2015-11-28 14:09:19 +0900

    デバイスの再接続に対応

--- MidiKeyboardTest/FormMain.cs
+++ MidiKeyboardTest/FormMain.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,20 +19,47 @@ namespace MidiKeyboardTest
             InitializeComponent();
         }
 
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WmDeviceChange)
+            {
+                new Thread(new ThreadStart(detectDevice)).Start();
+            }
+            else
+            {
+                base.WndProc(ref m);
+            }
+        }
+
+        private const int WmDeviceChange = 0x0219;
+
         private IntPtr midiIn;
         private Win32Midi.MidiInOpenCallback callbackDelegate;
+        private IntPtr callbackPtr;
 
         private void FormMain_Load(object sender, EventArgs e)
         {
             callbackDelegate = new Win32Midi.MidiInOpenCallback(callback);
+            callbackPtr = (IntPtr)Marshal.GetFunctionPointerForDelegate(callbackDelegate);
+            new Thread(new ThreadStart(detectDevice)).Start();
+        }
+
+        private delegate void addLogDelegate(string line);
+
+        private void addLog(string line)
+        {
+            textBoxDebug.Text += line + Environment.NewLine;
+        }
+
+        private Mutex mutexDetectDevice = new Mutex();
+
+        private void detectDevice()
+        {
+            mutexDetectDevice.WaitOne();
             var devNum = Win32Midi.MidiInGetNumDevs();
-            if (devNum == 0)
+            if (devNum != 0)
             {
-                textBoxDebug.Text = "利用できるMIDI入力デバイスが見つかりませんでした。";
-            }
-            else
-            {
-                var res = Win32Midi.MidiInOpen(ref midiIn, 0, (IntPtr)Marshal.GetFunctionPointerForDelegate(callbackDelegate), (IntPtr)0, Win32Midi.CallbackFunction);
+                var res = Win32Midi.MidiInOpen(ref midiIn, 0, callbackPtr, (IntPtr)0, Win32Midi.CallbackFunction);
                 if (res == Win32Midi.MMSysErrNoError)
                 {
                     res = Win32Midi.MidiInStart(midiIn);
@@ -41,9 +69,10 @@ namespace MidiKeyboardTest
                     var sb = new StringBuilder();
                     sb.Capacity = 1024;
                     Win32Midi.MidiInGetErrorText(res, sb, (uint)sb.Capacity);
-                    textBoxDebug.Text += sb.ToString() + Environment.NewLine;
+                    Invoke(new addLogDelegate(addLog), sb.ToString());
                 }
             }
+            mutexDetectDevice.ReleaseMutex();
         }
 
         private void callback(IntPtr hdrvr, uint msg, uint user, uint dw1, uint dw2)
@@ -51,7 +80,7 @@ namespace MidiKeyboardTest
             var state = (int)dw1 & 0xff;
             var data1 = (int)dw1 >> 8 & 0xff;
             var data2 = (int)dw1 >> 16 & 0xff;
-            textBoxDebug.Text += string.Format("{0}\t{1}\t{2}", state, data1, data2) + Environment.NewLine;
+            Invoke(new addLogDelegate(addLog), string.Format("{0}\t{1}\t{2}", state, data1, data2));
         }
 
         private void FormMain_FormClosed(object sender, FormClosedEventArgs e)
